b7a837ce21f9ec9ad41615c701b171f13cdbb555
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-10-25 12:53:48 -0300

    Add files via upload

---
+++ operadores-variaveis/app/src/main/java/com/adrianalatorre/variaveis/Operations.kt
@@ -0,0 +1,32 @@
+package com.adrianalatorre.variaveis
+
+class Operations {
+}
+
+//fun main() {
+   // var test = 1
+   // var convertion: Double = test.toDouble()
+//}
+
+//fun main() {
+   // var a = 5
+    //var b = 2
+    //var resultado = (1.0 * a) / b
+
+    //println(resultado)
+//}
+
+//fun main() {
+   // var texto = "5"
+   // var numero = 10
+   // var resultado = texto.toInt() - numero
+
+    //println(resultado)
+//}
+
+fun main() {
+    val name = "Adriana"
+    val template = "Hello world $name, number of letters is ${name.length}"
+
+    println(template)
+}
---
+++ operadores-variaveis/app/src/main/java/com/adrianalatorre/variaveis/Variables.kt
@@ -0,0 +1,13 @@
+package com.adrianalatorre.variaveis
+
+class Variables {
+}
+
+fun main() {
+    var resultadoSoma : Int
+    var inteiro = 1
+
+    val outroInteiro = 1
+
+
+}
