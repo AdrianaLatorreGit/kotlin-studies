91957432db8be3cdf92aa7e1c848e13f77677802
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-11-01 10:26:47 -0300

    Add files via upload

---
+++ estruturaDeRepeticao/app/src/main/java/com/adrianalatorre/estruturadedados_estruturaderepeticao/EstruturaRepeticao.kt
@@ -0,0 +1,14 @@
+package com.adrianalatorre.estruturadedados_estruturaderepeticao
+
+class EstruturaRepeticao {
+}
+
+//fun main() {
+//    var array: IntArray = IntArray(20)
+//
+//    for(i in array.indices){
+//        array[i] = i * 2
+//        println(array[i])
+//    }
+//}
+
